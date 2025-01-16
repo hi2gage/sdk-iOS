b59d34098df1df832ab0267ad18f73d2f5e5b7a1
hi2gage <hi2gage.pdx@gmail.com>
2025-01-17 00:47:56 +0700

    Add package.swift

---
+++ Improv-iOS/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Improv-iOS",
+    platforms: [.iOS(.v14)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ImproviOS",
+            targets: ["Improv-iOS"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Improv-iOS",
+            path: "."
+        ),
+        .testTarget(
+            name: "Improv-iOSTests",
+            dependencies: ["Improv-iOS"]
+        ),
+    ]
+)
